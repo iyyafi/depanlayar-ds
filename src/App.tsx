765dd5252a1d4eb9b9528caa498fdf92518c3587
iyyafi <iyyafi@gmail.com>
2024-11-17 19:01:50 +0700

    arrange component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { ButtonTailwind } from "./stories/ButtonTailwind";
+import { ButtonTailwind } from "./components/ButtonTailwind";
 
 function App() {
-	return (
-		<>
-			<h1>Vite + React</h1>
-			<ButtonTailwind label="Button" />
-		</>
-	);
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <ButtonTailwind label="Button" />
+    </>
+  );
 }
 
 export default App;
