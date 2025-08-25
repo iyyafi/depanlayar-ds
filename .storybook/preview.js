d265abf6b2349a187e0c3a556bd84db950b907b6
Iyyafi Qolbi <iyyafi@gmail.com>
2025-08-25 14:31:37 +0700

    change to html

---
+++ .storybook/preview.js
@@ -0,0 +1,16 @@
+/** @type { import('@storybook/html-vite').Preview } */
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    docs: {
+      codePanel: true,
+    },
+  },
+};
+
+export default preview;
